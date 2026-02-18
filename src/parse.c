a67708d8d323e5d845fc6ba235fa8bbff84a7613
Arsène Pérard-Gayot <arsene.perard@laposte.net>
2026-02-18 13:00:00 +0100

    Allow trailing commas
    This is not allowed in the grammar given in the spec, but oslc allows it.

--- src/parse.c
+++ src/parse.c
@@ -109,13 +109,11 @@ static struct ast* parse_many(
 {
     struct ast* first = NULL;
     struct ast** prev = &first;
-    if (parser->ahead->tag != stop) {
-        while (sep != TOKEN_ERROR || parser->ahead->tag != stop) {
-            *prev = parse_func(parser);
-            prev = &(*prev)->next;
-            if (sep != TOKEN_ERROR && !accept_token(parser, sep))
-                break;
-        }
+    while (parser->ahead->tag != stop && parser->ahead->tag != TOKEN_EOF) {
+        *prev = parse_func(parser);
+        prev = &(*prev)->next;
+        if (sep != TOKEN_ERROR && !accept_token(parser, sep))
+            break;
     }
     expect_token(parser, stop);
     return first;
